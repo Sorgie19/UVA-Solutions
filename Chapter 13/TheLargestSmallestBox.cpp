65a1971f1de99af3bf4cd0c9efe81bd90b6e545f
Sorgie <RTSorge@email.uark.edu>
2018-11-27 17:22:41 -0600

    Completed TheLargestSmallestBox UVA 10215

---
+++ Chapter 13/TheLargestSmallestBox.cpp
@@ -0,0 +1,25 @@
+#include <cmath>
+#include <cstdio>
+using namespace std;
+
+int main()
+{
+	double length;
+	double width;
+	double min;
+	double max;
+	while(scanf("%lf %lf",&length,&width)==2)
+	{
+		if(width < length)
+		{
+			min = (width / 2.0);
+		}
+		else
+		{
+			min = (length / 2.0);
+		}
+		max = (length + width - sqrt(length * length + width * width - length * width))/6.0;
+		printf("%.3f 0.000 %.3f\n",max + 1e-6, min + 1e-6);
+	}
+	return 0;
+}
