67ae4b583507040356c9cfb88e581aed55a4f4d5
Sorgie <RTSorge@email.uark.edu>
2018-12-01 22:36:11 -0600

    Finish Where's Waldorf | UVA 10010

--- Chapter 3/WheresWaldorf.cpp
+++ Chapter 3/WheresWaldorf.cpp
@@ -1,53 +1,104 @@
 #include <cstdio>
 using namespace std;
 
-char grid[51][51],word[51];
-int Case,m,n,k;
-
-void upper_to_lower (char a[])
-{
-    for (int i=0; a[i]; i++)
-        if (a[i]>='A' && a[i]<='Z')
-            a[i] += 32;
-}
-bool Locate (int i, int j)
+bool Locate(int a, int b, int row, int col, char grid[51][51], char word[51])
 {
     int x;
-    for (x=1; i-x>=0 && word[x]==grid[i-x][j];) {x++; if (word[x]=='\0') return 1;}
-    for (x=1; i+x<m  && word[x]==grid[i+x][j];) {x++; if (word[x]=='\0') return 1;}
-    for (x=1; j-x>=0 && word[x]==grid[i][j-x];) {x++; if (word[x]=='\0') return 1;}
-    for (x=1; j+x<n  && word[x]==grid[i][j+x];) {x++; if (word[x]=='\0') return 1;}
-    for (x=1; i-x>=0 && j-x>=0 && word[x]==grid[i-x][j-x];) {x++; if (word[x]=='\0') return 1;}
-    for (x=1; i+x<m  && j-x>=0 && word[x]==grid[i+x][j-x];) {x++; if (word[x]=='\0') return 1;}
-    for (x=1; i-x>=0 && j+x<n  && word[x]==grid[i-x][j+x];) {x++; if (word[x]=='\0') return 1;}
-    for (x=1; i+x<m  && j+x<n  && word[x]==grid[i+x][j+x];) {x++; if (word[x]=='\0') return 1;}
+    for (x = 1; a - x >= 0 && word[x] == grid[a - x][b];)
+    {
+        x++;
+        if (word[x] == '\0') return 1;
+    }
+    for (x = 1; a + x < row && word[x] == grid[a + x][b];)
+    {
+        x++;
+        if (word[x] == '\0') return 1;
+    }
+    for (x = 1; b - x >= 0 && word[x] == grid[a][b - x];)
+    {
+        x++;
+        if (word[x] == '\0') return 1;
+    }
+    for (x = 1; b + x < col && word[x] == grid[a][b + x];)
+    {
+        x++;
+        if (word[x] == '\0') return 1;
+    }
+    for (x = 1; a - x >= 0 && b - x >= 0 && word[x] == grid[a - x][b - x];)
+    {
+        x++;
+        if (word[x] == '\0') return 1;
+    }
+    for (x = 1; a + x < row && b - x >= 0 && word[x] == grid[a + x][b - x];)
+    {
+        x++;
+        if (word[x] == '\0') return 1;
+    }
+    for (x = 1; a - x >= 0 && b + x < col && word[x] == grid[a - x][b + x];)
+    {
+        x++;
+        if (word[x] == '\0') return 1;
+    }
+    for (x = 1; a + x < row && b + x < col && word[x] == grid[a + x][b + x];)
+    {
+        x++;
+        if (word[x] == '\0') return 1;
+    }
     return 0;
 }
+
+void casing(char word[])
+{
+    for (int i = 0; word[i]; i++)
+    {
+        if (word[i] >= 'A' && word[i] <= 'Z')
+        {
+            word[i] += 32;
+        }
+    }
+}
+
 int main()
 {
- //   freopen("input.txt","rt",stdin);
-    scanf("%d",&Case);
-    while (Case--){
-        scanf("%d%d",&m,&n);
-        for (int i=0; i<m; i++){
-            scanf("%s",grid[i]);
-            upper_to_lower(grid[i]);
+    int testCase;
+    int row;
+    int col;
+    int k;
+    char grid[51][51];
+    char word[51];
+    
+    scanf("%d", &testCase);
+    while (testCase--)
+    {
+        scanf("%d%d", &row, &col);
+        for (int i = 0; i < row; i++)
+        {
+            scanf("%s", grid[i]);
+            casing(grid[i]);
         }
 
-        scanf("%d",&k);
-        while (k--){
-            scanf("%s",word);
-            upper_to_lower(word);
-
-            int i,j,ok=0;
-            for (i=0; i<m && !ok; i++)
-                for (j=0; j<n && !ok; j++)
-                    if (grid[i][j] == word[0] && Locate (i,j)){
-                        printf("%d %d\n",i+1,j+1);
+        scanf("%d", &k);
+        while (k--)
+        {
+            scanf("%s", word);
+            casing(word);
+            int a;
+            int b;
+            int ok = 0;
+            for (a = 0; !ok && a < row; a++)
+                for (b = 0; !ok && b < col; b++)
+                {
+                    if (grid[a][b] == word[0] && Locate(a, b, row, col, grid, word))
+                    {
+                        printf("%d %d\n", a + 1, b + 1);
                         ok = 1;
                     }
+                }
         }
-        if (Case) printf("\n");
+        if (testCase)
+        {
+            printf("\n");
+        }
     }
     return 0;
 }
