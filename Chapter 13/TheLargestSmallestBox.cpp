8827bda2ce137d13607ed89f11d5ef85eac80667
Ryan Sorge <Rtsorge@email.uark.edu>
2019-10-07 14:58:22 -0500

    Update TheLargestSmallestBox.cpp

--- Chapter 13/TheLargestSmallestBox.cpp
+++ Chapter 13/TheLargestSmallestBox.cpp
@@ -11,13 +11,9 @@ int main()
 	while(scanf("%lf %lf",&length,&width)==2)
 	{
 		if(width < length)
-		{
 			min = (width / 2.0);
-		}
 		else
-		{
 			min = (length / 2.0);
-		}
 		max = (length + width - sqrt(length * length + width * width - length * width))/6.0;
 		printf("%.3f 0.000 %.3f\n",max + 1e-6, min + 1e-6);
 	}
